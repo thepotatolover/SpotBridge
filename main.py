10da4e6bda638c082af62da9e1035161396141cf
thepotatolover <142833759+thepotatolover@users.noreply.github.com>
2024-04-18 15:29:35 +0100

    Fix bug of it crashing

--- main.py
+++ main.py
@@ -33,22 +33,25 @@ RPC = Presence(discord_client_id,pipe=0)  # Initialize the client class
 RPC.connect() # Start the handshake loop
 
 while True:  # The presence will stay on as long as the program is running
-    current_track = sp.current_user_playing_track()
-    if current_track is not None:
-        track_name = current_track['item']['name']
-        artist_name = current_track['item']['artists'][0]['name']
-        album_name = current_track['item']['album']['name']
-        album_image = current_track['item']['album']['images'][0]["url"]
-        song_url = current_track['item']['external_urls']['spotify']
-        progress = milliseconds_to_minutes_seconds(current_track['progress_ms'])
-        duration = milliseconds_to_minutes_seconds(current_track['item']['duration_ms'])
-        explict = current_track['item']['explicit']
-        if explict == True:
-            explict = ', Explict: Yes'
+    try:
+        current_track = sp.current_user_playing_track()
+        if current_track is not None:
+            track_name = current_track['item']['name']
+            artist_name = current_track['item']['artists'][0]['name']
+            album_name = current_track['item']['album']['name']
+            album_image = current_track['item']['album']['images'][0]["url"]
+            song_url = current_track['item']['external_urls']['spotify']
+            progress = milliseconds_to_minutes_seconds(current_track['progress_ms'])
+            duration = milliseconds_to_minutes_seconds(current_track['item']['duration_ms'])
+            explict = current_track['item']['explicit']
+            if explict == True:
+                explict = ', Explict: Yes'
+            else:
+                explict = ''
+            print(RPC.update(details=f"{track_name}, {artist_name}", state=f"{progress}/{duration}{explict}", large_image=album_image, large_text=album_name, buttons=[{"label": "View on Spotify", "url": song_url}, {"label": "GitHub Repo", "url": 'https://github.com/thepotatolover/SpotBridge'}]))
+            time.sleep(15) # Can only update rich presence every 15 seconds
         else:
-            explict = ''
-        print(RPC.update(details=f"{track_name}, {artist_name}", state=f"{progress}/{duration}{explict}", large_image=album_image, large_text=album_name, buttons=[{"label": "View on Spotify", "url": song_url}, {"label": "GitHub Repo", "url": 'https://github.com/thepotatolover/SpotBridge'}]))
-        time.sleep(15) # Can only update rich presence every 15 seconds
-    else:
-        print(RPC.update(details=f"Not playing"))
-        time.sleep(35)
+            print(RPC.update(details=f"Not playing"))
+            time.sleep(35)
+    except Exception as e:
+        print(e)
